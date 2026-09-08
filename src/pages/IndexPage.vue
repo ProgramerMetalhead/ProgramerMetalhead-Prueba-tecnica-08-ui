666adab0db7465a84d72635c887edfab678a1a18
ProgramerMetalhead <Cristobal343@outlook.com>
2026-09-08 12:58:53 -0500

    Init commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    
+    <div class="text-center q-mb-lg">
+      <h4 class="text-h4">Registro de Prospectos</h4>
+      <p class="text-subtitle1">Ingresa los datos del nuevo cliente</p>
+    </div>
+    <!-- Inyección del componente -->
+    <ProspectForm />
+      
+  </q-page>
+</template>
+
+<script setup>
+// importa el componente ProspectForm
+import ProspectForm from '../components/ProspectForm.vue'
+</script>
