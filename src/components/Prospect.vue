46658097028be3802941bfdb047152bec170d730
ProgramerMetalhead <Cristobal343@outlook.com>
2026-09-08 14:05:05 -0500

    cambie la url

--- src/components/Prospect.vue
+++ src/components/Prospect.vue
@@ -54,7 +54,7 @@ const onSubmit = async () => {
 
     try {
         // Peticion al sevidor Laravel
-        const response = await fetch('http://127.0.0.1:420/api/prospects', {
+        const response = await fetch('http://127.0.0.1:480/api/prospects', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
